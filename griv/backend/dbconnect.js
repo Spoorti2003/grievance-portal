d0e69526e2c6ec0684e48bf20617f13913e97e3b
Spoorti Patil <spoortipatil2003@gmail.com>
2025-11-22 17:31:42 +0530

    Updated for deployment

--- griv/backend/dbconnect.js
+++ griv/backend/dbconnect.js
@@ -1,8 +1,9 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: "127.0.0.1",
-  user: "root",
-  password: "spoorti@2003",
-  database: "griv_com",
+  host: process.env.DB_HOST || "b0qxompucw36xbuz8jdi-mysql.services.clever-cloud.com",
+  user: process.env.DB_USER || "u8rqr2evyaarccbi",
+  password: process.env.DB_PASSWORD || "U15r3MMosNsION7NdWnP",
+  database: process.env.DB_NAME || "b0qxompucw36xbuz8jdi",
+  port: process.env.DB_PORT || 3306,
 });
--- griv/backend/server.js
+++ griv/backend/server.js
@@ -107,6 +107,7 @@ routes.post("/api/getgrives", fetchGrievances);
 app.use(routes);
 
 // Start the server
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
